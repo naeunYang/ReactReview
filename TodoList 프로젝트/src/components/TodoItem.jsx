42113d52980950aca012449c9346175560e6eef2
naeunYang <didskdms2@naver.com>
2025-06-05 16:21:34 +0900

    Initial commit

---
+++ TodoList 프로젝트/src/components/TodoItem.jsx
@@ -0,0 +1,27 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckBox} // input 요소이기 때문에 click이 아니라 change를 사용한다.
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+export default TodoItem;
